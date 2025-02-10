18b53f09ac8df03cf057e56c8d6d364da4884665
Vlada Sagermanova <sagermanovavl@gmail.com>
2025-02-11 02:50:19 +0300

    Написаны Интерфейсы и документация

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 // Интерфейс для описания товара
-export interface IProductItem {
+export interface IProduct {
     id: string;
     title: string;
     description: string;
@@ -8,35 +8,59 @@ export interface IProductItem {
     price: number | null;
   }
 
-//Интерфейс для описания объекта действий с одним методом onClick, который принимает объект MouseEvent
-  export interface IActions {
-    onClick: (event: MouseEvent) => void;
+//Интерфейс для вывода массива товаров
+  export interface IProductsList {
+    items: IProduct[];
   }
+
+// Тип содержащий данные товара используемые в корзине
+export type TProductBasket = Pick<IProduct, 'id' | 'title' | 'image' | 'price'>;
+
 
 // Интерфейс для описания формы заказа
 export interface IOrderForm {
-    name?: string;
-    email?: string;
-    phone?: string;
-    address?: string;
-    total?: string | number; // общая стоимость заказа
+    payment: string;
+    email: string;
+    phone: string;
+    address: string;
 }
 
-// Интерфейс расширяет IOrderForm и добавляет обязательное поле items, которое является массивом строк и представляет собой список идентификаторов товаров в заказе.
-export interface IOrder extends IOrderForm {
-  items: string[];
+// описывает ошибки формы заказа. Он использует утилиту Partial, чтобы сделать все поля необязательными, и утилиту Record, чтобы сопоставить ключи интерфейса IOrder со строками, представляющими ошибки
+export type FormErrors = Partial<Record<keyof IOrderForm, string>>;
+
+
+// Тип содержащий данные заказа, используемые в модальном окне cо способом оплаты и адресом
+export type TFormAddress = Pick<IOrderForm, 'payment' | 'address'>;
+
+// Тип содержащий данные заказа, используемые в модальном окне c электронной почтой и номером телефона
+export type TFormContacts = Pick<IOrderForm, 'email' | 'phone'>;
+
+// Интерфейс, описывающий данные и методы для работы с формами заказа
+export interface IOderFormsData {
+  formErrors: FormErrors;
+  setFormAddress(field: keyof TFormAddress, value: string): void; // установка поля формы адреса
+  setFormContacts(field: keyof TFormContacts, value: string): void; // установка поля формы контактов
+  validateFormAddress(): void; // валидация
+  validateFormContacts(): void;
+  clearFormAddress(): void; // очистка
+  cleaFormContacts(): void;
 }
 
-// интерфейс расширяет IOrder и делает все его свойства обязательными.
-export type RequiredOrder = Required<IOrder>;
-
-// Интерфейс описывает результат заказа
+// Интерфейс для описания результата заказа, содержащего идентификатор заказа и общую сумму
 export interface IOrderResult {
   id: string;
   total: number;
 }
 
-// описывает ошибки формы заказа. Он использует утилиту Partial, чтобы сделать все поля необязательными, и утилиту Record, чтобы сопоставить ключи интерфейса IOrder со строками, представляющими ошибки
-export type FormErrors = Partial<Record<keyof IOrder, string>>;
-
+//Интерфейс, описывающий коллекцию товаров в корзине
+export interface IProductBasketData {
+  items: TProductBasket[]; //массив товаров в корзине
+  getProductListInBasket(): TProductBasket[]; //получить массив товаров в корзине
+  getProductListInBasketNumber(): number;  // получить количество товаров  для отображения на корзине
+  addToBasket(): void; //добавить товар в корзину
+  removeFromBasket(): void; //удалить товар из корзины
+  getTotalPrice(): number; // получить сумму заказа
+  inBasket(): void; //проверить наличие товара в корзине 
+  clearBasketData(): void; //очистить данные корзины после заказа
+}
 
